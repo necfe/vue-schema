cafa00e568e96a6a554a8af9958a2592d8ca65eb
Rainfore <rainforest92@126.com>
2018-06-11 23:50:17 +0800

    :sparkles: Add an sidebar example

---
+++ demo/src/views/side.vue/index.js
@@ -0,0 +1,11 @@
+import VueSchema from 'vue-schema';
+import SchemaBase from 'vue-schema/src/base.vue';
+import data from './side.json';
+
+export default {
+    extends: SchemaBase,
+    created() {
+        const schema = new VueSchema(data);
+        schema.initialize({}, this);
+    },
+};
